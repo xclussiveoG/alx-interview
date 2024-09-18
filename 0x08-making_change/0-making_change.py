76cc5b91a004825d3aa26d7dc5b2b59bc445078d
xclussiveoG <ikireziremymagnom@gmail.com>
2024-09-18 04:05:53 -0700

    change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Making change."""
+
+
+def makeChange(coins, total):
+    """Solve the making change interview challenge."""
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
