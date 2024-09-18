8071d3f767eb12dc5b7b953f0081700978b8c0ad
xclussiveoG <ikireziremymagnom@gmail.com>
2024-09-18 04:11:48 -0700

    updated

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
-
-"""Making change interview challenge."""
+"""
+Coin Change Algorithm
+"""
 
 
 def makeChange(coins, total):
-    """Solve the making change interview challenge."""
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    """Calculate the fewest number needed to meet,
+    needed to meet a given total amount.
+    Args:
+        coins ([list]): A list of coin values available.
+        total ([number]): The target amount
+    Return: The fewest number of coins needed to reach the total,
+    or -1 if not possible.
+    """
+    if total <= 0:
+        return 0
 
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+    coins.sort(reverse=True)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    i, ncoins = (0, 0)
+    cpy_total = total
+    len_coins = len(coins)
+
+    while(i < len_coins and cpy_total > 0):
+        if (cpy_total - coins[i]) >= 0:
+            cpy_total -= coins[i]
+            ncoins += 1
+        else:
+            i += 1
+
+    check = cpy_total > 0 and ncoins > 0
+    return -1 if check or ncoins == 0 else ncoins
